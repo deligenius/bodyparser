ee7fff4f3e027bd65a64578e9d99c1755887ec7b
gjuoun <gjuoun@gmail.com>
2020-07-01 03:02:15 -0300

    update bound version deligenius@v1.0.0 multiparser@v1.0.0 std@0.59.0

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export { ServerRequest, Response as HttpResponse } from "https://deno.land/std/http/server.ts";
-export { posix, extname } from "https://deno.land/std/path/mod.ts";
+export { ServerRequest, Response as HttpResponse } from "https://deno.land/std@0.59.0/http/server.ts";
+export { posix, extname } from "https://deno.land/std@0.59.0/path/mod.ts";
 
-export {Context, Router} from "https://deno.land/x/deligenius/mod.ts"
-export { multiParser } from "https://deno.land/x/multiparser@v0.1.0/mod.ts"
+export {Context, Router} from "https://deno.land/x/deligenius@v1.0.0/mod.ts"
+export { multiParser } from "https://deno.land/x/multiparser@v1.0.0/mod.ts"
